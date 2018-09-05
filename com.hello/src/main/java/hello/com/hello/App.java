8a130134afc53aa1401e3194957888304b1b59a4
Ulyana Yeryksonava <uyerykso@its.jnj.com>
2018-09-05 12:54:35 +0300

    add simple Hello-world project

---
+++ com.hello/src/main/java/hello/com/hello/App.java
@@ -0,0 +1,13 @@
+package hello.com.hello;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
